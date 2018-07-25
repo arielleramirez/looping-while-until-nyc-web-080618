dd354eb8085b8c56fd4158aaa2e456f3d5411481
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 14:51:21 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,4 +1,12 @@
 def using_until
+  
+   z = 0
+  loop do
+    z+=1
+    puts "Hello Ruby-007!"
+    break if z == number_of_times
+  end
+end
         levitation_force = 0
 while levitation_force = 10
   puts "Wingardium Leviosa"
